d19de9a3c4d0cb03ef3f58874ac7212248015da3
Saith Kay <kaysaith@sina.com>
2018-02-25 02:12:23 +0800

    【界面】book map 所有全套界面完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,7 @@
 // pages/home/home.js
 
+import { Component } from '../../common/component'
+
 const covers = [
   'https://img3.doubanio.com/lpic/s29642615.jpg',
   'https://img1.doubanio.com/lpic/s29602519.jpg',
@@ -11,35 +13,52 @@ const covers = [
   'https://img3.doubanio.com/lpic/s29676191.jpg',
   'https://img3.doubanio.com/lpic/s29652156.jpg',
   'https://img3.doubanio.com/lpic/s29664110.jpg'
-  ]
+]
 
-  const names = [
-    "Say You're Sorry (Morgan Dane Book 1)",
-    "The Girl You Left Behind: A Novel",
-    "Before We Were Yours: A Novel",
-    "The Very Hungry Caterpillar Eric Carle",
-    "White Rose, Black Forest",
-    "Say You're Sorry (Morgan Dane Book 1)",
-    "The Girl You Left Behind: A Novel",
-    "Before We Were Yours: A Novel",
-    "The Very Hungry Caterpillar Eric Carle",
-    "White Rose, Black Forest"
-  ]
+const names = [
+  "Say You're Sorry (Morgan Dane Book 1)",
+  "The Girl You Left Behind: A Novel",
+  "Before We Were Yours: A Novel",
+  "The Very Hungry Caterpillar Eric Carle",
+  "White Rose, Black Forest",
+  "Say You're Sorry (Morgan Dane Book 1)",
+  "The Girl You Left Behind: A Novel",
+  "Before We Were Yours: A Novel",
+  "The Very Hungry Caterpillar Eric Carle",
+  "White Rose, Black Forest"
+]
 
-  const id = [
-    1920,
-    1921,
-    1922,
-    1923,
-    1924,
-    1925,
-    1926,
-    1927,
-    1928,
-    1929,
-  ]
+const id = [
+  1920,
+  1921,
+  1922,
+  1923,
+  1924,
+  1925,
+  1926,
+  1927,
+  1928,
+  1929,
+]
 
-  const data = []
+const positon = [
+  'column 3 row 2',
+  'column 2 row 6',
+  'column 4 row 8',
+  'column 6 row 1',
+  'column 1 row 3',
+  'column 3 row 2',
+  'column 2 row 6',
+  'column 4 row 8',
+  'column 6 row 1',
+  'column 1 row 3',
+]
+
+const data = []
+
+const item = []
+
+const app = getApp()
 
 Page({
 
@@ -48,7 +67,21 @@ Page({
    */
 
   data: {
-    showView: false
+    showView: false,
+    showSearchResult: false,
+    resultHeight: 0,
+    hasUploaded: false,
+    scrollViewHeight: 0,
+    showCreateButton: true,
+    showCancelButton: false
+  },
+
+  upper: function (e) {
+    console.log('hello')
+  },
+
+  scroll: function() {
+    console.log('shit')
   },
 
   /**
@@ -58,17 +91,28 @@ Page({
 
     for (let index = 0; index < 10; index++) {
       data.push({ 
-        src: covers[index] ,
+        src: covers[index],
         name: names[index],
         id: id[index]
+      })
+
+      item.push({
+        src: covers[index],
+        name: names[index],
+        position: positon[index]
       })
     }
     
     this.setData({
       array: data,
+      resultList: item
     })
 
-    showView: (options.showView == "true" ? true : false) 
+    this.setData({
+      scrollViewHeight: wx.getSystemInfoSync().windowHeight - 130,
+      resultHeight: wx.getSystemInfoSync().windowHeight - 130
+    })
+
   },
 
   // 显示悬浮曾的开关
@@ -76,6 +120,36 @@ Page({
     this.setData({
       showView: !this.data.showView
     })
+
+    // 悬浮曾关闭后恢复初始值
+    if (this.data.showView === false) this.setData({ hasUploaded: false })
+  },
+
+  showResult: function() {
+    this.setData({
+      showCancelButton: true,
+      showCreateButton: false,
+      showSearchResult: true
+    })
+  },
+
+  cancelSearch: function() {
+    this.setData({
+      showCancelButton: false,
+      showCreateButton: true,
+      showSearchResult: false
+    })
+  },
+
+  goToDetail: function (event) {
+    let pageInfo = JSON.stringify(data[event.currentTarget.dataset.index])
+    wx.navigateTo({
+      url: '../detail/detail?pageInfo=' + pageInfo
+    })
+  },
+
+  uploadCover: function() {
+    Component.chooseImage(() => this.setData({ hasUploaded: true }))
   },
 
   /**
