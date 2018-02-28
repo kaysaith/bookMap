2653056cd9a95d02b854fdea3d5adad3355d434b
Saith Kay <kaysaith@sina.com>
2018-02-28 17:28:09 +0800

    【功能】增加登录校验并上传给服务器用户信息

--- common/api.js
+++ common/api.js
@@ -2,12 +2,13 @@
  * Api Address
  */
 
-const isDebug = false
+const isDebug = true
 
 const header = 
   isDebug == false ? 'http://52.197.16.251:8888' : 'http://localhost:8888'
 
 export const Api = {
   uploadCover: header + '/upload',
-  createBook: header + '/createBook'
+  createBook: header + '/createBook',
+  getTokenAndUserInfo: header + '/getTokenAndUserInfo',
 }
