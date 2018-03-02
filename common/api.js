1edf5c08627f387da2289cf7793d81a4cf620583
Saith Kay <kaysaith@sina.com>
2018-03-02 16:49:45 +0800

    【功能】增加搜索功能

--- common/api.js
+++ common/api.js
@@ -2,10 +2,9 @@
  * Api Address
  */
 
-const isDebug = true
+const isDebugMode = true
 
-const header = 
-  isDebug == false ? 'http://52.197.16.251:8888' : 'http://127.0.0.1:8888'
+const header = !isDebugMode ? 'http://52.197.16.251:8888' : 'http://127.0.0.1:8888'
 
 export const Api = {
   uploadCover: header + '/upload',
@@ -16,5 +15,6 @@ export const Api = {
   getMemberList: header + '/getMemberList',
   deleteMember: header + '/deleteMember',
   modifyBookInfo: header + '/modifyBookInfo',
-  updateTargetBookInfo: header + '/updateTargetBookInfo'
+  updateTargetBookInfo: header + '/updateTargetBookInfo',
+  searchBook: header + '/searchBook'
 } 
